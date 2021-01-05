bdadd288a1520e401a2b8d033150d43dd5a430aa
Alexander Nikolaev <nikolaevsailing@mail.ru>
2021-01-05 13:34:24 +0400

    feat: players names

--- scripts/settings.js
+++ scripts/settings.js
@@ -2,6 +2,7 @@ export class Settings {
 	constructor() {
 		this.initialSettings = [
 			{ settingName: "playersCount", settingValue: "4" },
+			{ settingName: "playersNames", settingValue: ["Player1", "Player2", "Player3", "Player4"] },
 			{ settingName: "language", settingValue: "English" },
 			{ settingName: "sound", settingValue: "on" },
 			{ settingName: "color", settingValue: "red" }
