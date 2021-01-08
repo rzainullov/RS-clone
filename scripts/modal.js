e6cf9808889c38bba6f69ce665dec280f502c491
Alexander Nikolaev <nikolaevsailing@mail.ru>
2021-01-08 16:02:25 +0400

    refactor: change setting object

--- scripts/modal.js
+++ scripts/modal.js
@@ -26,17 +26,17 @@ export class Modal {
   }
 
   getSettings() {
-    if (localStorage.getItem("settings") === null) {
-      this.localSettings = [
+    this.localSettings = JSON.parse(localStorage.getItem("settings"))
+    || {
+      playerName: "Player1",
+      playerSettings: [
         { settingName: "playersCount", settingValue: "4" },
         { settingName: "playersNames", settingValue: ["Player1", "Player2", "Player3", "Player4"] },
         { settingName: "language", settingValue: "English" },
         { settingName: "sound", settingValue: "on" },
         { settingName: "color", settingValue: "red" }
-      ];
-    } else {
-      this.localSettings = JSON.parse(localStorage.getItem("settings"));
-    }
+      ]
+    };
     return this;
   }
 
@@ -53,7 +53,7 @@ export class Modal {
   }
 
   getWords(typeOfModal) {
-    const languageType = this.localSettings.find(el => el.settingName === "language").settingValue;
+    const languageType = this.localSettings.playerSettings.find(el => el.settingName === "language").settingValue;
     this.wordsArr = languages.find(el => el.langName === languageType)[typeOfModal];
     return this;
   }
@@ -165,7 +165,7 @@ export class Modal {
     this.createWrap();
 
     const wrapPlayers = document.createElement("div");
-    let playersCount = this.localSettings.find(el => el.settingName === "playersCount").settingValue;
+    let playersCount = this.localSettings.playerSettings.find(el => el.settingName === "playersCount").settingValue;
     wrapPlayers.innerText = `${this.wordsArr[2]} ${playersCount}`;
     wrapPlayers.classList.add("modal__text-item");
     this.wrap.appendChild(wrapPlayers);
@@ -173,7 +173,7 @@ export class Modal {
     const wrapNames = document.createElement("div");
     wrapNames.classList.add("modal__wrap-names");
     this.wrap.appendChild(wrapNames);
-    const playersNames = this.localSettings.find(el => el.settingName === "playersNames").settingValue;
+    const playersNames = this.localSettings.playerSettings.find(el => el.settingName === "playersNames").settingValue;
     const createAreaForNames = () => {
       wrapNames.innerHTML = "";
       const namesInput = [];
@@ -191,12 +191,12 @@ export class Modal {
     wrapPlayers.addEventListener("click", () => {
       playersCount = (playersCount % 4) + 1;
       wrapPlayers.innerText = `${this.wordsArr[2]} ${playersCount}`;
-      this.localSettings.find(el => el.settingName === "playersCount").settingValue = playersCount;
+      this.localSettings.playerSettings.find(el => el.settingName === "playersCount").settingValue = playersCount;
       createAreaForNames();
     });
 
     const wrapLanguage = document.createElement("div");
-    let languageType = this.localSettings.find(el => el.settingName === "language").settingValue;
+    let languageType = this.localSettings.playerSettings.find(el => el.settingName === "language").settingValue;
     wrapLanguage.innerText = `${this.wordsArr[3]} ${languageType}`;
     wrapLanguage.classList.add("modal__text-item");
     wrapLanguage.addEventListener("click", () => {
@@ -208,29 +208,29 @@ export class Modal {
         languageType = "English";
       }
       wrapLanguage.innerText = `${this.wordsArr[3]} ${languageType}`;
-      this.localSettings.find(el => el.settingName === "language").settingValue = languageType;
+      this.localSettings.playerSettings.find(el => el.settingName === "language").settingValue = languageType;
     });
     this.wrap.appendChild(wrapLanguage);
 
     const wrapSound = document.createElement("div");
-    let soundType = this.localSettings.find(el => el.settingName === "sound").settingValue;
+    let soundType = this.localSettings.playerSettings.find(el => el.settingName === "sound").settingValue;
     wrapSound.innerText = `${this.wordsArr[4]} ${soundType}`;
     wrapSound.classList.add("modal__text-item");
     wrapSound.addEventListener("click", () => {
       soundType = soundType === "on" ? "off" : "on";
       wrapSound.innerText = `${this.wordsArr[4]} ${soundType}`;
-      this.localSettings.find(el => el.settingName === "sound").settingValue = soundType;
+      this.localSettings.playerSettings.find(el => el.settingName === "sound").settingValue = soundType;
     });
     this.wrap.appendChild(wrapSound);
 
     const wrapColor = document.createElement("div");
-    let colorType = this.localSettings.find(el => el.settingName === "color").settingValue;
+    let colorType = this.localSettings.playerSettings.find(el => el.settingName === "color").settingValue;
     wrapColor.innerText = `${this.wordsArr[5]} ${colorType}`;
     wrapColor.classList.add("modal__text-item");
     wrapColor.addEventListener("click", () => {
       colorType = colorType === "red" ? "green" : "red";
       wrapColor.innerText = `${this.wordsArr[5]} ${colorType}`;
-      this.localSettings.find(el => el.settingName === "color").settingValue = colorType;
+      this.localSettings.playerSettings.find(el => el.settingName === "color").settingValue = colorType;
     });
     this.wrap.appendChild(wrapColor);
 
